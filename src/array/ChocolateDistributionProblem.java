0dcd5a3abe8cdf542e9ddc3a8d3117536451d540
jishan ansari <ansari_jishan@network.lilly.com>
2022-05-14 15:32:55 +0530

    added comments to  chocolate problem

--- src/array/ChocolateDistributionProblem.java
+++ src/array/ChocolateDistributionProblem.java
@@ -2,12 +2,15 @@ package array;
 
 import java.util.Arrays;
 
+/**
+ * Its a type  of subarray  problem solved  using sliding windoow algo
+ */
 public class ChocolateDistributionProblem {
 
     public static void main(String[] args) {
-        int[] numbers = {3, 4, 1, 9, 56, 7, 9, 12};
-        int arr_size = 7;
-        int m = 5;
+        int[] numbers = {12, 4, 7, 9, 2, 23, 25, 41, 30, 40, 28, 42, 30, 44, 48, 43, 50};
+        int arr_size = numbers.length;
+        int m = 7;
         int minDiff = findMinDiff(numbers, arr_size, m);
         System.out.printf("Minimum element is %d", minDiff);
     }
@@ -15,8 +18,32 @@ public class ChocolateDistributionProblem {
 
     public static int findMinDiff(int[] a, int n, int m) {
         // your code here
+        // if there are no chocolates or
+        // number of students is 0
+        if (m == 0 || n == 0)
+            return 0;
+
+        // Sort the given packets
         Arrays.sort(a);
-        int minDifference = a[n - 1];
+
+        // Number of students cannot be
+        // more than number of packets
+        if (n < m)
+            return -1;
+
+        //sorted array soo that  minDiffrence can be reduced
+        Arrays.sort(a);
+
+
+        int minDifference = Integer.MAX_VALUE;
+
+
+        // Find the subarray of size m
+        // such that difference between
+        // last (maximum in case of
+        // sorted) and first (minimum in
+        // case of sorted) elements of
+        // subarray is minimum.
         int i = 0;
         while (i < n - m + 1) {
             int min = a[i];

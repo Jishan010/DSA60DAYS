ffc71e975975d9816c0c9ba71987e030df866f15
jishan ansari <ansari_jishan@network.lilly.com>
2022-10-02 08:27:00 +0530

    solved some pattern questions using loop

--- src/array/PatterQuestions.java
+++ src/array/PatterQuestions.java
@@ -1,9 +1,9 @@
 package array;
 
-public class PrintMattrix {
+public class PatterQuestions {
     public static void main(String[] args) {
         /*printStarts(5);*/
-        printMatrix(5);
+        printMatrix2(6);
     }
 
     /*
@@ -39,14 +39,40 @@ public class PrintMattrix {
 
             //loop to print empty space
             for (int col2 = 1; col2 <= row; col2++) {
-                    System.out.print("*"+ " ");
+                System.out.print("*" + " ");
             }
 
             System.out.println();
 
         }
 
+    }
 
+
+    /**
+     * print below pattern
+     * *
+     * * *
+     * * * *
+     * * * * *
+     * * * * * *
+     *
+     * @param n
+     */
+    public static void printMatrix2(int n) {
+        //outer most for loop is for number of rows
+        for (int row = 1; row <= n; row++) {
+            //this for loop is for printing spaces
+            for (int col1 = 0; col1 < n - row; col1++) {
+                System.out.print(" ");
+            }
+
+            //this for loop is for printing stars
+            for (int col2 = 0; col2 < row; col2++) {
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
     }
 
 

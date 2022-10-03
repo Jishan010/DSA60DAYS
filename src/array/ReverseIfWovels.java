a6c29b3897fa97baf07a7da46d74f28206d19783
jishan ansari <ansari_jishan@network.lilly.com>
2022-10-03 07:32:01 +0530

    solved program on two pointers with while loop

--- src/array/ReverseIfWovels.java
+++ src/array/ReverseIfWovels.java
@@ -1,45 +1,48 @@
-/*
 package array;
 
-import java.util.List;
+
+import java.util.Arrays;
 
 public class ReverseIfWovels {
     public static void main(String[] args) {
 
+        String reversedValue = reverseVowels("This is my name");
+        System.out.println(reversedValue);
+
+        String reversedValue1 = reverseVowels("following");
+        System.out.println(reversedValue1);
     }
 
-
-    public static List<String> reverseVowels(List<Character> elements) {
+    // This is my name -> This as my nime
+    public static String reverseVowels(String elements) {
         int i = 0;
         int j = 1;
 
-        char[] items = elements.t();
+        char[] items = elements.toCharArray();
 
-        while (i < items.size()) {
-
-
-            if (checkIfWowel(items.get(i)) && checkIfWowel(items.get(j))) {
-
-
-                items.set()
-
+        while (j < items.length) {
+            if (checkIfWowel(items[i]) && checkIfWowel(items[j])) {
+                //logic for swap items
+                char temp = items[i];
+                items[i] = items[j];
+                items[j] = temp;
+                i = j;
+                j++;
+            } else if (checkIfWowel(items[i])) {
+                j++;
+            } else if (checkIfWowel(items[j])) {
+                i = j;
+                j = j + 1;
+            } else {
                 i++;
                 j++;
-
             }
-
-
         }
-
-
+        return Arrays.toString(items);
     }
 
 
     public static boolean checkIfWowel(char i) {
-        if (i == 'o' || i == 'i' || i == 'u' || i == 'a' || i == 'e') {
-            return true;
-        }
-        return false;
+        return i == 'o' || i == 'i' || i == 'u' || i == 'a' || i == 'e';
     }
 }
-*/

acd362c695b9c9f4587a1d0a3045412d2535a991
jishan ansari <ansari_jishan@network.lilly.com>
2022-10-08 08:46:48 +0530

    printed fibonacci series for a given number

--- src/array/PrintFibonacciValue.java
+++ src/array/PrintFibonacciValue.java
@@ -1,21 +1,36 @@
 package array;
 
+import java.util.Arrays;
+
 public class PrintFibonacciValue {
     public static void main(String[] args) {
-        System.out.println(getFibonacci(8));
+        System.out.println(Arrays.toString(getFibonacci(8)));
+        System.out.println(Arrays.toString(getFibonacci(33)));
     }
 
     // 0 1 1 2 3 5 8 13 21
-    public static int getFibonacci(int num) {
+    public static int[] getFibonacci(int num) {
         int a = 0;
         int b = 1;
-        int c = 0;
+        int c;
+        int[] fibonacciSeries = new int[num + 1];
+        fibonacciSeries[0] = 0;
+
+        if (num < 1) {
+            return fibonacciSeries;
+        }
+
+        fibonacciSeries[1] = 1;
+        if (num < 2) {
+            return fibonacciSeries;
+        }
 
         for (int i = 2; i <= num; i++) {
             c = a + b;
             a = b;
             b = c;
+            fibonacciSeries[i] = c;
         }
-        return c;
+        return fibonacciSeries;
     }
 }

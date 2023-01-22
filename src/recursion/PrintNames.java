77766ebcceae8e1004dcbb2ba002a9d181cb24de
Jishan Ansari <jishan010@gmail.com>
2023-01-22 15:29:01 +0530

    Solved some basic recursion problem

--- src/recursion/PrintNames.java
+++ src/recursion/PrintNames.java
@@ -3,7 +3,8 @@ package recursion;
 public class PrintNames {
 
     public static void main(String[] args) {
-        printNames(5);
+        int fact = factorial(5);
+        System.out.println(fact);
     }
 
     private static void printNames(int n) {
@@ -11,5 +12,29 @@ public class PrintNames {
             return;
         System.out.println("Jishan");
         printNames(n - 1);
+    }
+
+    //print numbers linearly 1 to N
+    private static void printNumbers(int n) {
+        if (n < 1)
+            return;
+        printNumbers(n - 1);
+        System.out.println(n);
+    }
+
+
+    //print number in  reverse order  from backtracking  n=3 , i = 1
+    private static void printReverseNumbers(int n, int i) {
+        if (i > n)
+            return;
+        printReverseNumbers(n, i + 1);
+        System.out.println(i);
+    }
+
+    private static int factorial(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n + factorial(n - 1);
     }
 }

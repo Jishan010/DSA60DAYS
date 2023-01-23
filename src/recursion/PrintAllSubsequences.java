d88106eca06e78346adfa1285faf7667f5f46d20
Jishan Ansari <jishan010@gmail.com>
2023-01-23 08:15:44 +0530

    Solved problem for subSequence for target Sum using recursive approach

--- src/recursion/PrintAllSubsequences.java
+++ src/recursion/PrintAllSubsequences.java
@@ -1,14 +1,16 @@
 package recursion;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class PrintAllSubsequences {
 
-
     public static void main(String[] args) {
-        int[] arr = {3, 2, 1};
-        int[] out = new int[arr.length];
-        printSubSequences(0, out, arr);
+        int[] arr = {1, 2, 1};
+        //int[] out = new int[arr.length];
+        List<Integer> result = new ArrayList<>();
+        subSequencesForTargetSum(0, result, 0, arr, 2);
     }
 
     public static void printSubSequences(int index, int[] outArray, int[] inArr) {
@@ -18,12 +20,35 @@ public class PrintAllSubsequences {
             return;
         }
 
-        //add the given index : for taking the given value
+        //take or pick the particular index into the subsequence
         outArray[index] = inArr[index];
         printSubSequences(index + 1, outArray, inArr);
 
-        //remove the added array  : for not taking the given value
+        //not pick or not take condition, this element is not added to your subsequent
         outArray[index] = 0;
         printSubSequences(index + 1, outArray, inArr);
     }
+
+    //program to get the subsequence who's some is equal to target sum
+    // for ex arr = {1,2,1} , targetSum = 2  Output = {1,1} and {2}
+    public static void subSequencesForTargetSum(int index, List<Integer> arrayList, int sum, int[] arr, int targetSum) {
+
+        if (index == arr.length) {
+            //limit reached : print the result
+            if (sum == targetSum) {
+                System.out.println(arrayList);
+            }
+            return;
+        }
+
+        //take or pick particular index into the subsequence
+        arrayList.add(arr[index]);
+        sum += arr[index];
+        subSequencesForTargetSum(index + 1, arrayList, sum, arr, targetSum);
+        sum -= arr[index];
+        arrayList.remove(arrayList.size() - 1);
+        //remove or do not pick condition, this element is not added to your subsequent
+        subSequencesForTargetSum(index + 1, arrayList, sum, arr, targetSum);
+    }
+
 }

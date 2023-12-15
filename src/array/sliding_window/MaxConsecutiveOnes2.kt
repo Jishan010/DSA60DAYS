3cd6cae466e7db46198d9298aac9ee13cb6eb81d
Jishan Ansari <jishan010@gmail.com>
2023-12-15 15:42:22 +0530

    corrected MaxConsecutiveOnes2 sliding window problem

--- src/array/sliding_window/MaxConsecutiveOnes2.kt
+++ src/array/sliding_window/MaxConsecutiveOnes2.kt
@@ -1,39 +1,34 @@
 package array.sliding_window
 
-
+//LC : 487
 fun main() {
 
     val nums = intArrayOf(1, 0, 1, 1, 0)
     println(findMaxConsecutiveOnes(nums))
 }
 
-
 fun findMaxConsecutiveOnes(nums: IntArray): Int {
     var i = 0
     var j = 0
     var countOfZeroes = 0
-    var maxConsecutiveOnes = Integer.MIN_VALUE
-    while (j < nums.size) {
+    var maxConsecutiveOnes = 0 // Initialize to 0
 
+    while (j < nums.size) {
         if (nums[j] == 0) {
             countOfZeroes++
         }
 
-        if (nums[i] == 0) {
-            countOfZeroes--
-        }
-
-        if (countOfZeroes == 2) {
-            //do the work
-            val windowSize = j - i + 1 - 1 ///last -1 is to remove zero count
-            maxConsecutiveOnes = maxOf(maxConsecutiveOnes, windowSize)
-
-            //slide the window
+        // If there are more than one 0 in the window, shrink the window
+        while (countOfZeroes > 1) {
+            if (nums[i] == 0) {
+                countOfZeroes--
+            }
             i++
-        } else {
-            j++
         }
-    }
 
+        // Calculate window size
+        maxConsecutiveOnes = maxOf(maxConsecutiveOnes, j - i + 1)
+        j++
+    }
     return maxConsecutiveOnes
 }

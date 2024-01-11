13565e71a3cca6cd46a0dd3dc44565a9be596498
Jishan Ansari <jishan010@gmail.com>
2024-01-11 14:57:02 +0530

    if applied, this commit will add a code implementation to solve anagram problem

--- src/array/FindDuplicates.kt
+++ src/array/FindDuplicates.kt
@@ -1,16 +1,16 @@
 package array
 
 fun main() {
-    val arr1 = intArrayOf(2, 3, 5, 6)
-    val arr2 = intArrayOf(1, 4, 2, 8)
+    /*  val arr1 = intArrayOf(2, 3, 5, 6)
+      val arr2 = intArrayOf(1, 4, 2, 8)
 
-    println(findDifference(arr1, arr2))
+      println(findDifference(arr1, arr2))*/
+    println(isAnagram("hello", "lolehh"))
 }
 
 fun findDifference(nums1: IntArray, nums2: IntArray): List<List<Int>> {
     val firstSet = nums1.toMutableSet()
     val secSet = nums2.toMutableSet()
-
     var answer0 = mutableListOf<Int>()
 
     for (num in firstSet) {
@@ -21,4 +21,30 @@ fun findDifference(nums1: IntArray, nums2: IntArray): List<List<Int>> {
         }
     }
     return mutableListOf(answer0, secSet.toList())
+}
+
+fun isAnagram(s1: String, s2: String): Boolean {
+
+    if (s1.length != s2.length)
+        return false
+
+    val hashMap = mutableMapOf<Char, Int>()
+    for (char in s1.toCharArray()) {
+        hashMap[char] = hashMap.getOrDefault(char, 0) + 1
+    }
+
+    for (char in s2.toCharArray()) {
+        if (hashMap.containsKey(char)) {
+            val elemt = hashMap.get(char)
+            if (elemt != null) {
+                if (elemt > 1) {
+                    hashMap[char] = hashMap.get(char)!! - 1
+                } else {
+                    hashMap.remove(char)
+                }
+            }
+        }
+    }
+
+    return hashMap.isEmpty()
 }
